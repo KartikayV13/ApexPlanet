40b4bc18b4191cc880e732dffc6c0227b3cd8834
Kartikay varun <varunkartikay0325@gmail.com>
2025-07-05 16:37:54 +0530

    todo-list completed

---
+++ T2/Todo/script.js
@@ -0,0 +1,20 @@
+function addTask () {
+    const inputText = document.querySelector(".inputText")
+    const taskText = inputText.value.trim();
+
+    if(taskText === "") return;
+
+    const li = document.createElement("li")
+    li.textContent = taskText;
+
+
+    const dltBtn = document.createElement("button")
+    dltBtn.textContent = "Delete";
+    dltBtn.classList.add("dlt-btn");
+    dltBtn.onclick = () => li.remove();
+
+    li.appendChild(dltBtn)
+    document.querySelector("#taskList").appendChild(li)
+
+    inputText.value="";
+}
